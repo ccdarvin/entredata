b4f459854c331317a3b0fe202d9a111a775dc445
Darvin Cotrina <cc.darvin@gmail.com>
2023-06-20 14:46:22 -0500

    change url

--- packages/docsWeb/docusaurus.config.js
+++ packages/docsWeb/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ccdarvin.github.io/',
+  url: 'https://docs.entredata.org/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
